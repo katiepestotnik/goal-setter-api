ec587bfb9ecbcf6b9e2ed69ea820710063f02188
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-12-24 17:08:21 -0700

    finished relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_24_231052) do
+ActiveRecord::Schema.define(version: 2021_12_25_000303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_12_24_231052) do
     t.text "goal_description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "update_id"
   end
 
   create_table "updates", force: :cascade do |t|
@@ -30,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_12_24_231052) do
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "goal_id"
   end
 
   create_table "users", force: :cascade do |t|
