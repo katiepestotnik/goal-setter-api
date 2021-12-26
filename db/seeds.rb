2abcdf80a05a07e668566aa55d734a993c8060dd
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-12-25 19:10:43 -0700

    fixed problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 10.times do
+#     User.create(username: "Bear", password: "mybeloved", email: "bear@gmail.com")
+# end
+# 10.times do
+#     Goal.create(name:"walk", start_date: Date.parse('2020-01-01', '%Y-%m-%d'), goal_completion_date: Date.parse('2020-01-01', '%Y-%m-%d'), goal_description: "walk around the block", user_id: 13)
+# end
 10.times do
-    User.create(username: "Bear", password: "mybeloved", email: "bear@gmail.com")
+    Update.create(actions: "walk", self_evaluation: 10, completed: true, goal_id: 9, user_id: 13)
 end
 
 
